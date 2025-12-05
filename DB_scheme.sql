b92ddf9a0a61f9969bfcf9bca49d7a2e098b13d9
Cheng0303 <guancheng0303@gmail.com>
2025-12-05 14:58:55 +0800

    DB_scheme.

--- DB_scheme.sql
+++ DB_scheme.sql
@@ -5,7 +5,7 @@ CREATE TABLE meetings (
   invite_code  VARCHAR(32) NOT NULL UNIQUE,                  -- 對外會議代碼：abc-defg-hij
   title        TEXT NOT NULL,
   date         DATE,                                         -- 你目前是字串，也可以先用 DATE
-  description  TEXT,                                         -- 想想要不要留
+  description  TEXT,                                         -- 想想要不要留.
   meet_url     TEXT,
   summary      TEXT,                                         -- 想想要不要留        
   created_at   TIMESTAMPTZ NOT NULL DEFAULT NOW(),
